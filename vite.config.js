83f8331231233f5f77585a4cf5ef3fdb83c3c656
komandor131 <koletttut@gmail.com>
2025-11-08 20:28:29 +0200

    Refactor Vite config to use glob for HTML input

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,19 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-// Імпорт glob прибираємо, бо він більше не потрібен
-// import { glob } from 'glob';
+import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-  
-    base: '/project_group4/', 
-    
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    // Коренева директорія проекту встановлена на 'src'
-    root: 'src', 
-    
+    root: 'src',
     build: {
       sourcemap: true,
       rollupOptions: {
-        // !!! ВИПРАВЛЕННЯ: Явно вказуємо єдиний вхідний HTML-файл 
-        // (шлях відносно 'root: src')
-        input: {
-          main: './index.html', 
-        },
+        input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
